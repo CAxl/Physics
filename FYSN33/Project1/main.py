f02dfb7f78c0ee21371f5109317c39090ae1ba99
Axel Cerne <peraxel01@gmail.com>
2026-02-05 10:47:36 +0100

    Project1 FYSN33 implementation of state vector and kernel

---
+++ FYSN33/Project1/main.py
@@ -0,0 +1,91 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class SPHsystem:
+    def __init__(self, N, kernel):
+        self.N = N # number of particles
+        self.kernel = kernel
+        
+        self.S = np.zeros((N,4)) # s_i = [x_i, v_i, \rho_i, e_i]
+        self.m = np.ones(N) # all masses equal
+
+    # getters
+    # defines index convention as:
+    # S[i][0] = x_i, S[i][1] = v_i, S[i][2] = \rho_i, S[i][3] = e_i
+    # in other words, initialize S using this convention please!
+    @property
+    def x(self):
+        return self.S[:, 0]
+
+    @property
+    def v(self):
+        return self.S[:, 1]
+
+    @property
+    def rho(self):
+        return self.S[:, 2]
+
+    @property
+    def e(self):
+        return self.S[:, 3]    
+    
+    # define the geometry, dx and r = |x_i - x_j|. How to move to 3D in r?
+    # note that dx is signed!
+    def geom(self):
+        dx = self.x[:,None] - self.x[None,:]
+        r = np.abs(dx)
+
+        return dx, r
+    
+    # density summation
+    def density_summation(self):
+        dx, r = self.geom()
+        W = self.kernel.W(r)
+
+        # fill state vector with densities (rho[:] == S[:,2])
+        self.rho[:] = W @ self.m
+        assert np.all(self.rho > 0) and np.isfinite(self.rho).all()
+        return self
+
+
+class cubicSplineKernel:
+    # implements the cubic spline kernel and its analytical derivative
+    # (Gaussian looking function, but goes to zero explicitly)
+    # It measures the region of influence particle at x_i exerts on particle at x_j
+
+    def __init__(self, h):
+        self.h = h
+        self.a_d = 1/h # for now
+    
+    def W(self,r):
+        R = np.abs(r)/self.h    # R_ij = |x_i-x_j|/h
+        f1 = lambda R: (2/3) - R**2 + 0.5*R**3
+        f2 = lambda R: (1/6) * (2 - R)**3
+
+        return self.a_d * np.piecewise(R, [(R>= 0) & (R<1), (R>=1) & (R<=2)], [f1,f2,0.0])
+
+    def gradW(self, dx, r):
+
+        R = np.abs(r)/self.h
+        f1 = -2*R + 1.5 * R**2
+        f2 = -0.5 * (2 - R)**2
+
+        dWdr = self.a_d * np.piecewise(R, [(R>= 0) & (R<1), (R>=1) & (R<=2)], [f1,f2,0.0])
+
+        # check diags non nan
+        gradW = dWdr * np.sign(dx)
+        assert np.allclose(np.diag(gradW), 0.0)
+
+        return gradW
+    
+
+
+    
+
+
+
+
+
+
+
---
+++ FYSN33/Project1/testing.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import main
+
+
+# # Broadcasting
+# x = np.linspace(7, 45, 100)
+# y = np.linspace(90, 5, 100)
+
+# coly = y[:,np.newaxis] # shape as column vector
+
+# # Broadcasting sum
+# t0 = time.time()
+# summ = x[:,np.newaxis] + y
+# t1 = time.time()
+
+# print("this took",  round(t1 - t0, 4), "s")
+
+# # Double loop for matrix sum
+# t0 = time.time()
+# sumxy = np.zeros((len(x), len(y)))
+# for i in range(len(x)):
+#     for j in range(len(y)):
+#         sumxy[i][j] = x[i] + y[j]        
+# t1 = time.time()
+
+# print("looping sum takes ", round(t1 - t0, 4), "seconds")
+
+
+# print("=========================================================")
+# print(summ)
+# print("=========================================================")
+# print(sumxy)
+
+
+
+
+# testing N particles with kernels
+
+N = 100
+kernel = main.cubicSplineKernel(h=1.0)
+sys = main.SPHsystem(N, kernel)
+sys.S[:,0] = np.linspace(-10,10,N)
+
+dx, r = sys.geom()
+
+W = kernel.W(r)
+print(W)
+
+x = sys.x
+
+for i in range(sys.N):
+    plt.plot(x, W[i,:], marker = 'o', label=f"kernel at x[{i}] = {x[i]:.1f}")
+    plt.axvline(x[i], linestyle='--', alpha = 0.5)
+
+plt.xlabel("absolute position, x")
+plt.ylabel("W(|x_i-x_j|)")
+plt.legend()
+plt.title("felt interaction for particle x_j due to particle x_i")
+plt.show()
+
+# plot the 10:th particle's kernel and the 89:th particle's kernel
+plt.plot(x, W[10,:], marker = 'o',label=f"kernel at x[{10}] = {x[10]:.1f}")
+plt.plot(x, W[90,:], marker = 'o',label=f"kernel at x[{90}] = {x[90]:.1f}")
+plt.axvline(x[10], linestyle = '--', alpha = 0.5)
+plt.axvline(x[90], linestyle = '--', alpha = 0.5)
+plt.xlabel("x")
+plt.ylabel("W(r)")
+plt.legend()
+plt.show()
+
+
+
+# density summation test
+sys.density_summation()
+rho = sys.rho
+print()
+print("density vector = ", rho)
+
+
